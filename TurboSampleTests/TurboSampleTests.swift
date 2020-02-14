3b444b9dbe52004e168d8a5f034f7b2e1831ac4f
Michael Voline <mvoline@ebay.com>
2020-02-13 21:38:01 -0500

    added tests for coordinator

--- TurboSampleTests/TurboSampleTests.swift
+++ TurboSampleTests/TurboSampleTests.swift
@@ -9,26 +9,21 @@
 import XCTest
 @testable import TurboSample
 
-class TurboSampleTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class AccountingCoordinatorTests: XCTestCase {
+    
+    var subject: AccountingCoordinator!
+    
+    func testCoordination() {
+        let navigation = UINavigationController()
+        
+        subject = AccountingCoordinator(navigation: navigation)
+       
+        subject.didPressSettings(currency: "test", events: SettingsViewController.Events(logoutHandler: {
+            
+        }, currencyChangeHandler: { _ in
+        
+        }))
+        
+        XCTAssertTrue(navigation.topViewController!.isKind(of: SettingsViewController.self))
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
